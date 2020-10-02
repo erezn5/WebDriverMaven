35855a6c5459ec3010051eff488d71d0c9d809b6
erezn5 <erezn5@gmail.com>
2020-10-02 13:28:59 +0300

    Initial Commit

---
+++ src/test/java/webdriverMaven/LoginTest.java
@@ -0,0 +1,35 @@
+package webdriverMaven;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LoginTest {
+	public static WebDriver driver;
+	
+	@BeforeSuite
+	public void setUp() {
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+	}
+	
+	@Test
+	public void doLogin() throws InterruptedException {
+		driver.get("http://gmail.com");
+		driver.findElement(By.id("identifierId")).sendKeys("trainer@way2automation.com");
+		driver.findElement(By.id("identifierNext")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.name("password")).sendKeys("sfafgasd");
+	}
+	
+	@AfterSuite
+	public void teardown() {
+		driver.quit();
+	}
+
+}
